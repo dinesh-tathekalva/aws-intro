f1b25a1923b40e2459bdcb7cf7da7e839ba409ad
Ravi Botla <ravikiran.botla@gmail.com>
2020-05-03 15:09:09 -0700

    Sun May  3 15:09:09 PDT 2020

--- 3-aws-cli-node-web/server.js
+++ 3-aws-cli-node-web/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const getIP = require('external-ip')();
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  getIP((err, ip) => {
+    if (err) throw err;
+    res.send('Hello World! from - ', ip)
+  });
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
