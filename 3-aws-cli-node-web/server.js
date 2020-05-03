1fe40f13181411ced013bc439f8e6fc50f1f1d75
Ravi Botla <ravikiran.botla@gmail.com>
2020-05-03 12:59:40 -0700

    Sun May  3 12:59:40 PDT 2020

--- 3-aws-cli-node-web/server.js
+++ 3-aws-cli-node-web/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const moment = require('moment');
+
 const app = express()
 
 const apiKey = '*****************';
@@ -26,7 +28,7 @@ app.post('/', function (req, res) {
         res.render('index', {domains: null, error: 'Error, please try again'});
       } else {
         //et weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        const domains = resp.domains.map(d => ({domain: d.domain, createdOn: d.create_date, country: d.country, isActive: !d.isDead}));
+        const domains = resp.domains.map(d => ({domain: d.domain, createdOn: moment(d.create_date, "MM-DD-YYYY"), country: d.country, isActive: !d.isDead}));        
         res.render('index', {domains: domains, error: null});
       }
     }
