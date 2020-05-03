6d93170bfd0e940e35f7ed2bc982c9d54e2d9e14
Ravi Botla <ravikiran.botla@gmail.com>
2020-05-03 10:52:28 -0700

    Sun May  3 10:52:28 PDT 2020

--- 3-aws-cli-node-web/README.md
+++ 3-aws-cli-node-web/README.md
@@ -3,48 +3,20 @@
 ssh -i ravi-aws-web.pem ec2-user@<ip-address>
 ```
 
-> Install Node using NVM
-```
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node -e "console.log('Running Node.js ' + process.version)"
-```
-
 > Install git client
 ```
 sudo yum install git -y
 ```
 
+> Clone this repository in EC2 machine
 ```
-mkdir app
-cd app
-npm init -y
-npm in
+git clone https://github.com/rbotla/aws-intro.git
+cd aws-intro
 ```
 
-
-> Intall
-
-
+> Install Node using the install-node.sh script
 ```
-<<EOF > index.js
-
-var http = require('http')
-var url = require('url')
-
-http.createServer(onRequest).listen(3000);
-console.log('Server has started');
-
-function onRequest(request, response){
-  var pathName = url.parse(request.url).pathname
-  console.log('pathname' + pathName);
-  response.writeHead(200);
-  response.write('Hello Noders');
-  response.end();
-}
-
-EOF
-
+install-node.sh
 ```
+> 
 
---
+++ 3-aws-cli-node-web/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const apiKey = '*****************';
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let domain = req.body.domain;
+  let url = `https://api.domainsdb.info/v1/domains/search?domain=${domain}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {createdOn: null, error: 'Error, please try again'});
+    } else {
+      let resp = JSON.parse(body)
+      if(resp.domains == undefined){
+        res.render('index', {c: null, error: 'Error, please try again'});
+      } else {
+        //et weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        const domains = resp.domains.map(d => ({domain: d.domain, createdOn: d.create_date, country: d.country, isActive: !d.isDead}));
+        res.render('index', {domains: domains, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Domain app listening on port 3000!')
+})
