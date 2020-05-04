983905744e6ca9fad8efc6a2bd63e39c5c0f7929
Ravi Botla <ravikiran.botla@gmail.com>
2020-05-04 16:11:41 -0700

    Mon May  4 16:11:41 PDT 2020

--- 4-ec2-node-web-with-db/server.js
+++ 4-ec2-node-web-with-db/server.js
@@ -57,6 +57,6 @@ app.listen(3000);
 function formatHTML(ideas) {
   return ideas.map(i => {
     const is = `<div style="width:300px; border: 1px solid blue; padding: 10px; margin: 10px;">${i.idea}</div>`;
-    return is.join('<br />');
+    return is.join('\n');
   })
 } 
